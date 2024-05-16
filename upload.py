c64b67edd35f42437a14abf7c45db2da25cb78c2
Leoni <leoni.dessau@mailbox.org>
2024-05-16 16:27:16 +0200

    upload script kinda ugly

--- upload.py
+++ upload.py
@@ -2,16 +2,22 @@
 
 import serial
 import subprocess
+import threading
+import time
 
 serial_con = serial.Serial('/dev/ttyS0', 9600, timeout=1)
 
+def reset_arduino():
+    while True:
+        # send the reset command to the arduino, so it resets
+        serial_con.write('R 10\n'.encode('utf-8'))
+        time.sleep(0.5)
+
+
+reset_thread = threading.Thread(target=reset_arduino)
+reset_thread.start()
 # first compile the arduino sketch, so that upload has a more predictable
 # runtime
-subprocess.run(['mkdir', '-p', 'build'])
-subprocess.run(['arduino-cli', 'compile', 'nim-psu.ino', '-b', 'arduino:avr:nano', '--output-dir', './build'])
+subprocess.run(['arduino-cli', 'compile', 'nim-psu.ino', '-b', 'arduino:avr:nano', '-u', '-p', '/dev/ttyS03'])
 
-# send the reset command to the arduino, so it resets
-# after 500ms,
-serial_con.write('R 500\n'.encode('utf-8'))
-
-subprocess.run(['arduino-cli', 'upload', './build/', '-b', 'arduino:avr:nano', '-p', '/dev/ttyS0'])
+reset_thread.join()
